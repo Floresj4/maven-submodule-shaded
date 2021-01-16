6fca07ec862aa8f7bcf1b67a6a8664e12ced78db
Jason <floresj4@gmail.com>
2021-01-15 20:21:41 -0500

    add models sub modules

---
+++ shaded-models/src/main/java/com/flores/development/shaded/models/SomeObject.java
@@ -0,0 +1,11 @@
+package com.flores.development.shaded.models;
+
+/**
+ * A model object to be shared across modules
+ * and other external projects that may need
+ * them.
+ * @author Jason
+ */
+public class SomeObject {
+	
+}
