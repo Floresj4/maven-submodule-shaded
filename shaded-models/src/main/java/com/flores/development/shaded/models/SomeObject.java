9ee0d27b8ea007a4656c893e05b7c16b193ca473
Jason <floresj4@gmail.com>
2021-01-15 20:51:54 -0500

    adding lombok and annotating model object

--- shaded-models/src/main/java/com/flores/development/shaded/models/SomeObject.java
+++ shaded-models/src/main/java/com/flores/development/shaded/models/SomeObject.java
@@ -1,11 +1,28 @@
 package com.flores.development.shaded.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 /**
  * A model object to be shared across modules
  * and other external projects that may need
  * them.
  * @author Jason
  */
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class SomeObject {
 	
+	@Setter
+	private String propertyA;
+
+	@Setter
+	private int properyB;
+	
+	@NonNull @Getter
+	private String initProperty;
 }
