e07575fd9f957639fc54b6a4acb8249f39160f11
Jason <floresj4@gmail.com>
2021-01-15 21:06:44 -0500

    adding interface and models pom reference

---
+++ shaded-logic-v1/src/main/java/com/flores/development/shaded/logic/ILogicExecution.java
@@ -0,0 +1,17 @@
+package com.flores.development.shaded.logic;
+
+import com.flores.development.shaded.models.SomeObject;
+
+/**
+ * Default interface for versioned logic implementations
+ * to be bound to.
+ * @author Jason
+ */
+public interface ILogicExecution {
+	
+	/**
+	 * Execute some business logic
+	 * @param something to execute on
+	 */
+	public void execute(SomeObject something);
+}
